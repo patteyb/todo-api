e946b8caee9e207f87125b7b826973d3537a7710
Pattey Bleecker <patteybleecker@gmail.com>
2016-08-18 12:24:21 -0400

    Refactor with underscore

--- server.js
+++ server.js
@@ -1,48 +1,57 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
 var todoNextId = 1;
 
-// this loads the middleware allowing me to
-// get the parsed json with req.body
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-   res.send('Todo API Root'); 
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
 });
 
-app.get('/todos', function(req, res) {
-    res.json(todos);
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
 });
 
-app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    // Keep only the fields we want
+	var body = _.pick(req.body, 'task', 'completed');
+  
+	if (!_.isBoolean(body.completed) || !_.isString(body.task) || body.task.trim().length === 0) {
+		return res.status(400).send();
+	}
     
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+    // normalize incoming data
+    body.task = body.task.trim();
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
 });
 
-app.post('/todos', function(req, res) {
-    var body = req.body;
-    body.id = todoNextId++;
-    todos.push(body);
-    
-    res.send(body);
-});
- 
-app.listen(PORT, function() {
-   console.log('Express listening on port ' + PORT + "..."); 
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '...');
 });
