6b2b5237e40f5b788dc2e6a47579cb411c69c0d2
Pattey Bleecker <patteybleecker@gmail.com>
2016-08-18 15:09:30 -0400

    Add Filtered Get

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ todos = [{
 }, {
     id: 3,
     task: "Do homework",
-    completed: false
+    completed: true
 }];
 
 app.use(bodyParser.json());
@@ -29,8 +29,22 @@ app.get('/', function (req, res) {
 });
 
 // GET /todos
+// This will work for all get requests, whether
+// they have a query or not
 app.get('/todos', function (req, res) {
-	res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    
+    // If has property && completed === true
+    // filteredTodos = _.where(filterTodos, ?)
+    // else if has prop && completed if false
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    };
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
