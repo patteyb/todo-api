25c337774190480fe71da5e30d83e1f4405b2f15
Pattey Bleecker <patteybleecker@gmail.com>
2016-08-24 17:30:50 -0400

    Add user model and post/user request

--- server.js
+++ server.js
@@ -118,6 +118,20 @@ app.put('/todos/:id', function(req, res) {
     });
 });
 
+/* USER FUNCTIONS ----------------------------------------------------- */
+
+// POST /user
+app.post('/user', function (req, res) {
+    // Keep only the fields we want
+	var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+		res.json(user.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+});
+
 db.sequelize.sync().then( function() {
     app.listen(PORT, function () {
 	    console.log('Express listening on port ' + PORT + '...');
